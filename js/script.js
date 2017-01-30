457e2929e0ee77e5cb293af1db28fb10639fca41
Justin Jo <justinj7@gmail.com>
2017-01-30 00:54:07 -0500

    Added logoff messages

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 var uuid = PubNub.generateUUID();
 
 var pubnub = new PubNub({
- 	subscribeKey: 'sub-c-22d37992-dea5-11e6-b2ae-0619f8945a4f',
+    subscribeKey: 'sub-c-22d37992-dea5-11e6-b2ae-0619f8945a4f',
     publishKey: 'pub-c-211da9ce-adcf-44ab-b2d1-fc1c0a8eaadc',
     uuid: uuid
 });
@@ -27,13 +27,13 @@ function (status, response) {
         // handle error
         console.log(status)
     } else {
-        console.log("message Published w/ timetoken", response.timetoken)
+        console.log('message Published w/ timetoken', response.timetoken)
     }
 }
 );
 
 function send_message() {
-    var message = document.getElementById("usermessage").value;
+    var message = document.getElementById('usermessage').value;
     if (message.length > 0) {
         pubnub.publish({
             message : {
@@ -43,16 +43,33 @@ function send_message() {
             channel : 'Chat Channel',
         });
     }
-    document.getElementById("usermessage").value = "";
+    document.getElementById('usermessage').value = '';
 }
 
+function send_message(m) {
+    pubnub.publish({
+        message : {
+            uuid : uuid,
+            text : m
+        },
+        channel : 'Chat Channel',
+    });
+}
+
+// update chat box
 (function() {
     pubnub.addListener({
         message: function(m) {
-            var chat = document.getElementById("chatbox");
+            var chat = document.getElementById('chatbox');
             // first 8 characters of UUID determines username
-            chat.innerHTML = chat.innerHTML + 'User ' + m.message.uuid.substring(0,8) + ': \'' + m.message.text + '\'<br>';
+            var message = 'User ' + m.message.uuid.substring(0,8) + ': \'' + m.message.text + '\'<br>';
+            chat.innerHTML = chat.innerHTML + message;
             console.log(m)
+            //chat.scrollTop = element.scrollHeight;
         }
     })
+
+    window.onbeforeunload = function() {
+        send_message('Logging off')
+    }
 })();
